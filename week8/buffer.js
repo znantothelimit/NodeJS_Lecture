0a20d43fbf2ad2220292c8dcf76d1a71a94f8281
kekemon <56341387+znantothelimit@users.noreply.github.com>
2023-04-27 16:50:43 +0900

    week8 lecure
    week8 lecure

---
+++ week8/buffer.js
@@ -0,0 +1,11 @@
+const buffer = Buffer.from('abcde\n');
+console.log('from():', buffer); // ASCII CODE 16진수형식으로 출력
+console.log('length:', buffer.length);
+console.log('toString():', buffer.toString());
+
+const array = [Buffer.from('띄엄 '), Buffer.from('띄엄 '), Buffer.from('띄어쓰기')];
+const buffer2 = Buffer.concat(array);
+console.log('concat():', buffer2.toString());
+
+const buffer3 = Buffer.alloc(5);
+console.log('alloc():', buffer3);
---
+++ week8/fsDelete.js
@@ -0,0 +1,21 @@
+//////// fsDelete.js 먼저 실행후 실행
+const fs = require('fs');
+
+fs.readdir('./folder', (err, dir) => {
+  if (err) {
+    throw err;
+  }
+  console.log('폴더 내용 확인', dir);
+  fs.unlink('./folder/newfile.js', (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('파일 삭제 성공');
+    fs.rmdir('./folder', (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log('폴더 삭제 성공');
+    });
+  });
+});
